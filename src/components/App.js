088b4a746f55d54036cd1ffa61fd8082c115cdc0
Ema Lorenzo <aemalorenzo@gmail.com>
2019-03-20 20:59:09 -0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import styled from 'styled-components';
+import { useSpring, animated } from 'react-spring';
+import Typography from '@material-ui/core/Typography';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import SocialIcons from './SocialIcons';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
+const App = () => (
+  <MuiThemeProvider theme={theme}>
+    <Root />
+  </MuiThemeProvider>
+);
+
+const Card = styled(animated.div)`
+  width: 100ch;
+  height: 60ch;
+  border-radius: 20px;
+  background-image: linear-gradient(-225deg, #3D4E81 0%, #5753C9 48%, #6E7FF3 100%);
+  background-size: cover;
+  background-position: center center;
+  box-shadow: 0px 10px 30px -5px rgba(0, 0, 0, 0.3);
+  transition: box-shadow 0.5s;
+  will-change: transform;
+  /* border: 15px solid white; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  &:hover {
+    box-shadow: 0px 30px 100px -10px rgba(0, 0, 0, 0.4);
+  }
+`;
+
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.03];
+const trans = (x, y, s) => `perspective(1000px) rotateX(${x / 4}deg) rotateY(${y / 4}deg) scale(${s})`;
+
+const Root = () => {
+  const [springProps, set] = useSpring(
+    () => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }),
+  );
+  return (
+    <Card
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      style={{ transform: springProps.xys.interpolate(trans) }}
+    >
+      <Typography component="h2" variant="h1" gutterBottom>
+        Hi!
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        I am Emanuel Lorenzo, a mobile developer
+      </Typography>
+      <SocialIcons />
+    </Card>
+  );
+};
+
+export default App;
---
+++ src/components/SocialIcons.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import IconButton from '@material-ui/core/IconButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faGithub, faLinkedin, faInstagram, faFacebook,
+} from '@fortawesome/free-brands-svg-icons';
+
+const SocialIcons = styled.div`
+
+`;
+
+export default () => (
+  <SocialIcons>
+    <IconButton target="_blank" href="https://github.com/emaLorenzo">
+      <FontAwesomeIcon icon={faGithub} />
+    </IconButton>
+    <IconButton target="_blank" href="https://www.linkedin.com/in/emanuellorenzo">
+      <FontAwesomeIcon icon={faLinkedin} />
+    </IconButton>
+    <IconButton target="_blank" href="http://instagram.com/aemalorenzo">
+      <FontAwesomeIcon icon={faInstagram} />
+    </IconButton>
+    <IconButton target="_blank" href="https://www.facebook.com/aemalorenzo">
+      <FontAwesomeIcon icon={faFacebook} />
+    </IconButton>
+  </SocialIcons>
+);
