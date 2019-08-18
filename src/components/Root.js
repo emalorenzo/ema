7e86bc30cc6b6c56b00ede269bda8deec413c4a4
Ema Lorenzo <aemalorenzo@gmail.com>
2019-08-18 19:40:03 -0300

    More fixes for CI

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
